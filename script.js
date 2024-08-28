bedee69085e8e728e4ffd86a069c5a77866f4eb7
PororoBot <138274724+PororoBot@users.noreply.github.com>
2024-08-28 10:33:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -84,10 +84,9 @@ function testWin(){
 		(slot2 == slot3 && slot2 == "a7") ) && !(slot1 == slot2 && slot2 == slot3 && slot1=="a7")){
 		status.innerHTML = "YOU WIN!";
 		win.play();
-	}else{
-		status.innerHTML = "YOU LOSE!"
-		lose.play();
-	}
+	else 
+		status.innerHTML = "YOU WIN!";
+		win.play();
 	doing = false;
 }
 
